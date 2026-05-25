24f6a8c41f6e2c111f83457ef6a4fdc57ee600dd
him8nshu777 <himanshuraut0968@gmail.com>
2026-05-25 17:15:15 +0530

    correct media files

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -39,9 +39,9 @@ urlpatterns = [
 # =========================================================
 # MEDIA FILES
 # =========================================================
-if settings.DEBUG:
+# if settings.DEBUG:
 
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT,
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT,
     )
